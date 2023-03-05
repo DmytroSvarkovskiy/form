a2a0f3c65a35d7caaac143098ecaf9e595f4a3fe
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-03-05 23:12:10 +0200

    -

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,17 @@
 import { lazy } from 'react'
-import { GlobalStyle } from './GlobalStyle'
+import { GlobalStyle } from '../GlobalStyle'
 import { Route, Routes } from 'react-router-dom'
-import { Layout } from './components'
-import { Dashboard } from './pages'
+import { Layout } from '.'
+import { Dashboard } from '../pages'
 const EstablishmentCurrent = lazy(
-  () => import('./pages/EstablishmentCurrent/EstablishmentCurrent'),
+  () => import('../pages/EstablishmentCurrent/EstablishmentCurrent'),
 )
-const Categories = lazy(() => import('./pages/Categories/Categories'))
+const Categories = lazy(() => import('../pages/Categories/Categories'))
 const CategoriesInModeration = lazy(
-  () => import('./pages/CategoriesInModeration/CategoriesInModeration'),
+  () => import('../pages/CategoriesInModeration/CategoriesInModeration'),
 )
-const Customers = lazy(() => import('./pages/Customers/Customers'))
-const Roles = lazy(() => import('./pages/Roles/Roles'))
+const Customers = lazy(() => import('../pages/Customers/Customers'))
+const Roles = lazy(() => import('../pages/Roles/Roles'))
 
 const App: React.FC = () => {
   return (
